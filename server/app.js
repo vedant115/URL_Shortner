78e7adf2c544684b537d721df0c69003dcd395fb
vedant115 <vedant.a.rahane@gmail.com>
2025-06-22 21:50:51 +0530

    add modular backend files

--- server/app.js
+++ server/app.js
@@ -1,28 +1,22 @@
 import express from "express";
-import { nanoid } from "nanoid";
 import dotenv from "dotenv";
+
 import connectDB from "./src/config/mongo.config.js";
-import urlSchema from "./src/models/shorturl.model.js";
-dotenv.config("./.env");
+import shortUrl from "./src/routes/shortUrl.route.js";
+import { redirectToOriginalUrl } from "./src/controller/shortUrl.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/api/create", (req, res) => {
-  const { url } = req.body;
-  const shorturl = nanoid(7);
-  const newShortUrl = new urlSchema({
-    originalUrl: url,
-    shortUrl: shorturl,
-  });
-  newShortUrl.save();
-  res.status(201).json({
-    message: "Short URL created successfully",
-    shortUrl: shorturl,
-  });
-});
+app.use("/api/create", shortUrl);
+app.get("/:id", redirectToOriginalUrl);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   connectDB();
