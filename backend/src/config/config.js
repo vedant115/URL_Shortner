beddd894a3ff89ef2dde983f9039f702b17de6ea
vedant115 <vedant.a.rahane@gmail.com>
2025-06-24 01:13:12 +0530

    update cookie config

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,7 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "Lax",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   maxAge: 1000 * 60 * 60, // 1 hour
   path: "/",
 };
