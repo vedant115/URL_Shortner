e1b59f735ddf2afa4a7131fb561f831707781038
vedant115 <vedant.a.rahane@gmail.com>
2025-06-23 22:20:09 +0530

    fix useState import error

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 
